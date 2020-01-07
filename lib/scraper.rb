c93c6d4a1bab194154a4a9f6057cd40538b34cad
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-07 20:13:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,8 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))  
+    
+    binding.pry 
   end
   
   def print_courses
@@ -23,5 +25,5 @@ class Scraper
   
 end
 
-
+Scraper.new.get_page
 
